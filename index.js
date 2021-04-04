70c70c9b7c12530559dbe1ca74ca8b3c9469758a
Tyler Bray <broncobray77@gmail.com>
2021-04-04 14:08:14 -0700

    fizzbuzz

--- index.js
+++ index.js
@@ -95,3 +95,17 @@ var maximumWealth = function(accounts) {
     
   return count
 };
+
+function fizzbuzz(){
+  for(var i = 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+      console.log("fizzbuzz")
+    } else if (i % 3 == 0) {
+      console.log("fizz")
+    } else if (i % 5 == 0) {
+      console.log("buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
