14ed3f8949e8b086bbc6007f670b6a0db4b0438a
Tyler Bray <broncobray77@gmail.com>
2021-05-05 16:05:58 -0700

    greet

--- index.js
+++ index.js
@@ -238,3 +238,8 @@ function lovefunc(flower1, flower2){
   // moment of truth
   return !(flower1 % 2 === 0 && flower2 % 2 === 0 || flower1 % 2 != 0 && flower2 % 2 != 0);
 }
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+}
